af973d3a61297ff75f04fa939b4040444fa5b31c
Lena Y <lenayoungydq@gmail.com>
2021-03-16 18:35:35 +0100

    npm start

--- from-zero/webpack.config.js
+++ from-zero/webpack.config.js
@@ -1,3 +1,5 @@
+
+
 const path = require("path");
 // export default {
 module.exports = {
@@ -8,16 +10,16 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: '/',
     },
-    resolve: {
-        extensions: ['.js','.jsx']
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?/,
-            loader:'babel-loader'
-        }]
-    }
+    // resolve: {
+    //     extensions: ['.js','.jsx']
+    // },
+    // devServer: {
+    //     historyApiFallback: true
+    // },
+    // module: {
+    //     rules: [{
+    //         test: /\.jsx?/,
+    //         loader:'babel-loader'
+    //     }]
+    // }
 }
